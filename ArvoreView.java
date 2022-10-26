880d5ef584cec999a98f59e4c644d978f0b376dc
Gcld <gabrielcostalima21@gmail.com>
2022-10-26 16:40:25 -0300

    Update ArvoreView.java

--- ArvoreView.java
+++ ArvoreView.java
@@ -3,14 +3,29 @@ package imd.edb;
 public class ArvoreView {
 	public static void main(String[] args) {
 		Tree tree = new Tree();
-		//int[] items = {6, 1, 2, 3, 4, 5, 0, 7, 8, 9};
 		int[] items = {5, 8, 6, 3, 9};
-		for (int i : items)
-			tree.insert(i);
 		
-		System.out.println("Tree Height: " + tree.getAltura());
-		System.out.println("Tree Size: " + tree.getTamanho());
+		for (int i : items) {
+			tree.insert(i);
+		}
+		
+		tree.imprimeArvore(1);
+		tree.imprimeArvore(2);
+			
+		System.out.println("Arvore Altura: " + tree.getAltura());
+		System.out.println("Arvore Tamanho: " + tree.getTamanho());
+		
 		tree.preordem();
+		
+		tree.remove(8);
+		tree.remove(6);
+		
+		tree.preordem();
+		
+		System.out.println();
+		
+		System.out.println("Arvore Altura: " + tree.getAltura());
+		System.out.println("Arvore Tamanho: " + tree.getTamanho());
 		
 	}
 }
