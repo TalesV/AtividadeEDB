1a9fbc92e182f35d2e7664da655e56d1cb70193d
TalesV <92830980+TalesV@users.noreply.github.com>
2022-10-28 15:25:21 -0300

    +EnesimoElemento - Altura,Tamanho

--- ArvoreView.java
+++ ArvoreView.java
@@ -3,7 +3,7 @@ package imd.edb;
 public class ArvoreView {
 	public static void main(String[] args) {
 		Tree tree = new Tree();
-		int[] items = {5, 8, 6, 3, 9};
+		int[] items = {5, 8, 6, 9, 3, 2, 4, 1};
 		
 		for (int i : items) {
 			tree.insert(i);
@@ -12,22 +12,17 @@ public class ArvoreView {
 		tree.imprimeArvore(1);
 		tree.imprimeArvore(2);
 			
-		System.out.println("Arvore Altura: " + tree.getAltura());
-		System.out.println("Arvore Tamanho: " + tree.getTamanho());
-		System.out.println("Quantidade de Nodes: " + tree.contadorNodes());
+		//System.out.println("Quantidade de Nodes: " + tree.contadorNodes());
 		
 		tree.preordem();
-		
-		tree.remove(8);
-		tree.remove(6);
 		
 		tree.preordem();
 		
 		System.out.println();
 		
-		System.out.println("Arvore Altura: " + tree.getAltura());
-		System.out.println("Arvore Tamanho: " + tree.getTamanho());
-		System.out.println("Quantidade de Nodes: " + tree.contadorNodes());
+		System.out.println(tree.enesimoElemento(8));
+		
+		//System.out.println("Quantidade de Nodes: " + tree.contadorNodes());
 		
 	}
 }
