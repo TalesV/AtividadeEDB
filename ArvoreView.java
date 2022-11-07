720e69f84c2388e19e88bb5044f68f6eeb50084c
Gcld <gabriel.lima.712@ufrn.edu.br>
2022-11-07 16:27:26 -0300

    Comentários adicionados

--- ArvoreView.java
+++ ArvoreView.java
@@ -1,18 +1,23 @@
 package imd.edb;
 
+// Bibliotecas necessárias para leitura dos arquivos.
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
 public class ArvoreView {
     public static void main(String[] args) throws IOException {
     	
-        //Instancia a arvore
+        // Instancia a arvore
         Arvore tree = new Arvore();
         
+        // Leitura do arquivo1
     	Path arquivo1 = Paths.get("arquivo1.txt");
     	List<String> linhasArquivo1 = Files.readAllLines(arquivo1);
+    	
+    	// Insere os números do arquivo1 na ABB.
     	for(String linha : linhasArquivo1) {
     		String[] splitedLinha = linha.split(" ");
     		for(String num : splitedLinha) {
@@ -21,10 +26,11 @@ public class ArvoreView {
     		}
     	}
 
-        
+        // Leitura do arquivo2
         Path arquivo2 = Paths.get("arquivo2.txt");
         
         List<String> linhasArquivo2 = Files.readAllLines(arquivo2);
+        // Chama os métodos listados no arquivo2 através do tree.metodo. 
         for(String linha : linhasArquivo2) {
         	String[] splitedLinha = linha.split(" ");
         	if(splitedLinha.length == 1)
